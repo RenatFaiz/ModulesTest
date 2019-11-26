0652b84d4eea09b58087f59934f4046f81e0424e
RenatFaiz <renatf87@gmail.com>
2019-11-26 12:31:23 +0400

    Servlet added (config via web.xml)

---
+++ app/src/main/java/ru.itpark/servlet/FrontServlet.java
@@ -0,0 +1,16 @@
+package ru.itpark.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class FrontServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello servlets!");
+    }
+}
